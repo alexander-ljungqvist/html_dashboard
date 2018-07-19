192118eaaa3e373466c0bc88576488bcae403092
Alex2 <al.ljungqvist@gmail.com>
2018-07-19 10:21:40 +0200

    Fully functioning logic
    Logic is now fully functioning after recreating scripts inside VUE
    classes. Also minor cleanup in html and CSS.

--- js/vue.js
+++ js/vue.js
@@ -1,16 +1,37 @@
-var door = new Vue({
+new Vue({
   el: '#doorev',
   data: {
     events: []
-  }
-})
+  },
+  methods: {
+    doorevents() {
+      setInterval(() => {
+        var self = this;
+        getInfo("http://172.16.119.201:8080", function (request) {
+          var response = request.currentTarget.response;
+          var body = JSON.parse(response);
+          body = JSON.parse(body.mqtt_body);
+          self.events = [];
+          var today = moment(1, "HH"); // 01:00 todayƒ
+          for (var i = 0; i < body.length; i++) {
+            if (body[i].Topic == "Securitas/DoorAccessGranted") {
+              var m = moment(body[i].Payload.DateTime, 'YY/MM/DD hh:mm:ss');
+              if (m.isAfter(today)) { // remove if object is not today
+                self.events.push(body[i]);
+              }
+            }
+          }
+          self.events = self.events.reverse();
+        });
+      }, 10000);
 
-var konf = new Vue({
-  el: '#konf',
-  data: {
-    rooms: {}
+    },
+  },
+  mounted: function () {
+    this.doorevents();
   }
-})
+});
+
 
 new Vue({
   el: '#activeusers',
@@ -18,11 +39,12 @@ new Vue({
     users: []
   },
   methods: {
-    time() {
+    activeusers() {
       setInterval(() => {
         var self = this
+        var userArr = [];
         self.users = [];
-        self.getInfo("https://jsonplaceholder.typicode.com/posts", function (request) {
+        getInfo("https://jsonplaceholder.typicode.com/posts", function (request) {
           var response = request.currentTarget.response;
           var jsonUser = JSON.parse(response);
           for (var i = 0; i < jsonUser.length; i++) {
@@ -35,15 +57,58 @@ new Vue({
       }, 10000);
 
     },
-    getInfo(addr, sucess) {
-      var xhr = new XMLHttpRequest();
-      if (!('withCredentials' in xhr)) xhr = new XDomainRequest();
-      xhr.open('GET', addr);
-      xhr.onload = sucess;
-      xhr.send();
+  },
+  mounted: function () {
+    this.activeusers();
+  }
+});
+
+new Vue({
+  el: '#konf',
+  data: {
+    rooms: {}
+  },
+  methods: {
+    bookings() {
+      roomnames = [];
+      myrooms = {};
+      var self= this;
+      getInfo("http://172.16.8.230:3000/api/rooms", function (request) {
+        var response = request.currentTarget.response;
+        var roomresponse = JSON.parse(response);
+        for (room of roomresponse) {
+          roomnames.push(room.name);
+          myrooms[room.fullname] = [];
+        }
+      });
+      setTimeout(function () {
+        for (room of roomnames) {
+          getInfo("http://172.16.8.230:3000/api/rooms/" + room + "/events?to=" + moment(23, "HH").toISOString(), function (request) {
+            var body = JSON.parse(request.currentTarget.response);
+            for (var i = 0; i < body.length; i++) {
+              body[i].timeStart = moment(body[i].timeStart).format("HH:mm")
+              body[i].timeEnd = moment(body[i].timeEnd).format("HH:mm")
+            }
+            if (body.length != 0) {
+              myrooms[body[0].room] = body;
+            }
+          });
+        }
+        setTimeout(function () {
+          self.rooms = myrooms;
+        }, 5000);
+      }, 1000);
     },
   },
   mounted: function () {
-    this.time();
+    this.bookings();
   }
 });
+
+function getInfo(addr, sucess) {
+  var xhr = new XMLHttpRequest();
+  if (!('withCredentials' in xhr)) xhr = new XDomainRequest();
+  xhr.open('GET', addr);
+  xhr.onload = sucess;
+  xhr.send();
+}
