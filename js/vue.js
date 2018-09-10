6bea23d75b709d927b23c89f8f1c190798646a01
Alexander Ljungqvist <al.ljungqvist@gmail.com>
2018-09-10 13:02:20 +0200

    Added functionality
    For camera and calls to API

--- js/vue.js
+++ js/vue.js
@@ -1,4 +1,11 @@
 'use strict'
+var camera_array = {
+  //Get all names of the cameras and make changes when something changes.
+  PYR01_PyrEntrance :"isMarkedPyr01",
+  PYR04_PyramidV1Entrance: "isMarkedPyr04",
+  PET01_PetraEntrance: "isMarkedPet01"
+}
+
 new Vue({
   el: '#doorev',
   data: {
@@ -8,7 +15,7 @@ new Vue({
     doorevents() {
       setInterval(() => {
         var self = this;
-        getInfo("http://172.16.119.201:8080", function (request) {
+        getInfo("http://172.16.119.201:8080", function(request) {
           var response = request.currentTarget.response;
           var body = JSON.parse(response);
           body = JSON.parse(body.mqtt_body);
@@ -28,12 +35,11 @@ new Vue({
 
     },
   },
-  mounted: function () {
+  mounted: function() {
     this.doorevents();
   }
 });
-
-
+//HEll
 new Vue({
   el: '#activeusers',
   data: {
@@ -42,12 +48,12 @@ new Vue({
   methods: {
     activeusers() {
       setInterval(() => {
-        var self = this
+        var self = this;
         var userArr = [];
-        self.users = [];
-        getInfo("https://jsonplaceholder.typicode.com/posts", function (request) {
+        getInfo("http://127.0.0.1:5000/api/activeusers", function(request) {
           var response = request.currentTarget.response;
           var jsonUser = JSON.parse(response);
+          console.log(jsonUser);
           for (var i = 0; i < jsonUser.length; i++) {
             userArr.push(jsonUser[i]);
           }
@@ -55,11 +61,11 @@ new Vue({
           userArr = [];
 
         });
-      }, 10000);
+      }, 20000);
 
     },
   },
-  mounted: function () {
+  mounted: function() {
     this.activeusers();
   }
 });
@@ -75,33 +81,76 @@ new Vue({
         var self = this;
         var roomnames = [];
         var myrooms = {};
-        getInfo("http://172.16.8.230:3000/api/rooms", function (request) {
+        getInfo("http://172.16.8.230:3000/api/rooms", function(request) {
           var response = request.currentTarget.response;
           var roomresponse = JSON.parse(response);
           for (var i = 0; i < roomresponse.length; i++) {
             roomnames.push(roomresponse[i].name);
             myrooms[roomresponse[i].fullname] = [];
           }
-     
-        for (var i = 0; i < roomnames.length; i++) {
-          getInfo("http://172.16.8.230:3000/api/rooms/" + roomnames[i] + "/events?to=" + moment(23, "HH").toISOString(), function (request) {
-            var body = JSON.parse(request.currentTarget.response);
-            for (var i = 0; i < body.length; i++) {
-              body[i].timeStart = moment(body[i].timeStart).format("HH:mm")
-              body[i].timeEnd = moment(body[i].timeEnd).format("HH:mm")
-            }
-            if (body.length != 0) {
-              myrooms[body[0].room] = body;
-            }
-            self.rooms = myrooms;
-          });
-        }
-      });
+
+          for (var i = 0; i < roomnames.length; i++) {
+            getInfo("http://172.16.8.230:3000/api/rooms/" + roomnames[i] + "/events?to=" + moment(23, "HH").toISOString(), function(request) {
+              var body = JSON.parse(request.currentTarget.response);
+              for (var i = 0; i < body.length; i++) {
+                body[i].timeStart = moment(body[i].timeStart).format("HH:mm")
+                body[i].timeEnd = moment(body[i].timeEnd).format("HH:mm")
+              }
+              if (body.length != 0) {
+                myrooms[body[0].room] = body;
+              }
+              self.rooms = myrooms;
+            });
+          }
+        });
       }, 10000);
     },
   },
-  mounted: function () {
+  mounted: function() {
     this.bookings();
+  }
+});
+
+new Vue({
+  el: '#camera_response_msg',
+  data: {
+    arrays_:{
+      isMarkedPet01: false,
+      isMarkedPet02: false,
+      isMarkedPet03: false,
+      isMarkedPet04: false,
+      isMarkedPet05: false,
+      isMarkedPet06: false,
+      isMarkedPyr01: false,
+      isMarkedPyr02: false,
+      isMarkedPyr03: false,
+      isMarkedPyr04: false,
+      isMarkedPyr05: false,
+      isMarkedPyr06: false,
+    },
+    
+  },
+  methods: {
+    camera_response_update() {
+      setInterval(() => {
+        var self = this;
+        getInfo("http://127.0.0.1:8081/mqtt_response", function(request) {
+
+          var camera_decider = request.currentTarget.response;
+          var getProperty = function (propertyName) {
+            return camera_array[propertyName];
+        };
+        console.log(self.arrays_.isMarkedPet01)
+        var camera = getProperty(camera_decider);
+        console.log(camera);
+     
+        });
+      }, 5000);
+
+    },
+  },
+  mounted: function() {
+    this.camera_response_update();
   }
 });
 
@@ -109,6 +158,9 @@ function getInfo(addr, sucess) {
   var xhr = new XMLHttpRequest();
   if (!('withCredentials' in xhr)) xhr = new XDomainRequest();
   xhr.open('GET', addr);
+  xhr.onerror= function(){
+    console.log("Currently no changes available");
+  }
   xhr.onload = sucess;
   xhr.send();
 }
